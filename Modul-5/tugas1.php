e852eb7c16d1af10828d4feefc7fc487f3c4b29a
mahdiaanisrina <64676348+mahdiaanisrina@users.noreply.github.com>
2020-11-15 03:31:21 +0700

    Prakweb2 Modul 5

---
+++ Modul-5/tugas1.php
@@ -0,0 +1,35 @@
+<?php 
+
+	$bil1 = 20;
+	$bil2 = 5;
+
+	echo "Bilangan 1 = 20<br>";
+	echo "Bilangan 2 = 5<br><br>";
+	echo "Berikut merupakan hasil dari setiap operasi<br><br>";
+
+	echo "PENJUMLAHAN <br>";
+	echo "Operator : + <br>";
+	$jumlah = $bil1 + $bil2;
+	echo "Hasil : ".$jumlah."<br><br>";
+
+	echo "PENGURANGAN <br>";
+	echo "Operator : - <br>";
+	$min = $bil1 - $bil2;
+	echo "Hasil : ".$min."<br><br>";
+
+	echo "PERKALIAN <br>";
+	echo "Operator : * <br>";
+	$kali = $bil1 * $bil2;
+	echo "Hasil : ".$kali."<br><br>";
+
+	echo "PEMBAGIAN <br>";
+	echo "Operator : / <br>";
+	$bagi = $bil1 / $bil2;
+	echo "Hasil : ".$bagi."<br><br>";
+
+	echo "MODULUS <br>";
+	echo "Operator : % <br>";
+	$mod = $bil1 % $bil2;
+	echo "Hasil : ".$mod."<br><br>";
+
+ ?>
